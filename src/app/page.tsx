d16769e5a013e1bd3c47b2c78b3ff0c2c2ecfd4e
Brandon Cruz-Youll <42663258+BeeSeeWhy@users.noreply.github.com>
2025-01-17 19:41:39 -0800

    used next image rather than img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex items-center space-x-4">
-          <img src="/images/maintenance-icon.jpg" alt="Logo" width="200" />
+          <Image src="/images/maintenance-icon.jpg" alt="Logo" width="200" />
           <div className="text-4xl sm:text-6xl font-bold">
             Welcome to Equipmaint
           </div>
